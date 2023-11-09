99beed5037c7a5748c6784cdb0ec86120c856633
Adam <90611877+Adam1416k@users.noreply.github.com>
2023-11-09 15:34:09 +0100

    files

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll', function() {
+    const boxes = document.querySelectorAll('.box');
+    const windowHeight = window.innerHeight;
+
+    boxes.forEach(function(box) {
+        const boxTop = box.getBoundingClientRect().top;
+        const boxBottom = box.getBoundingClientRect().bottom;
+
+        // Om boxens överkant är närmare än fönstrets höjd -50px (skapar en liten fördröjning) OCH
+        // om boxens nederkant är större än 0 (d.v.s. boxen är fortfarande inom synfältet)
+        if (boxTop < windowHeight - 50 && boxBottom > 0) {
+            box.style.opacity = '1';
+            box.style.transform = 'translateY(0)';
+        } else {
+            box.style.opacity = '0';
+            box.style.transform = 'translateY(50px)';
+        }
+    });
+});
+
+document.querySelector('.menu-button').addEventListener('click', function() {
+    var menu = document.querySelector('.menu');
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    } else {
+        menu.classList.add('active');
+    }
+});
